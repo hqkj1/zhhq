cb12ad2f66d0ec754b881cce75fddcff393b0cdb
RenrenWang <1011420784@qq.com>
2017-10-13 13:37:36 +0800

    dasdasda

--- src/containers/NavBar/NavBar.vue
+++ src/containers/NavBar/NavBar.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="layout">
-      <router-view></router-view>
- </div>
+  <header class="nav-bar">
+         <span></span>
+  
+  </header>
 </template>
 
 <script>
 export default {
-  name: 'Layout',
+  name: '',
   data () {
     return {
       
@@ -17,5 +18,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-@import "../../assets/style/base.scss";
+@import "../assets/style/base.scss";
 </style>
